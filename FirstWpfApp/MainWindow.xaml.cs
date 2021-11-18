b0f1262fb5be07322c13ee037c71d36308838fa0
Aleksander Bekrenev <praise2001@yandex.ru>
2021-11-18 09:19:30 +0700

    Initial commit

---
+++ FirstWpfApp/MainWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using FirstWpfApp.ViewModels;
+
+namespace FirstWpfApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var mainWindowViewModel = new MainWindowViewModel();
+            this.DataContext = mainWindowViewModel;
+
+            int biggerFont = 32;
+            int smallerFont = 15;
+
+            layoutGrid.Background = new SolidColorBrush(Color.FromRgb(58, 0x8b, 0xED));
+            
+            DecorateTextBlock(MethodNameTextBlock, biggerFont, false);
+            MethodNameTextBlock.VerticalAlignment = VerticalAlignment.Bottom;
+            
+            DecorateTextBlock(ChooseFunctionTextBlock, biggerFont, false);
+            DecorateTextBlock(BoundsTextBlock, biggerFont, false);
+            
+            DecorateTextBlock(LeftBoundTextBlock, smallerFont, false);
+            DecorateTextBlock(RightBoundTextBlock, smallerFont, false);
+            
+            DecorateTextBlock(AccuracyTextBlock, smallerFont, false);
+            DecorateTextBlock(minumumTextBlock, smallerFont, true);
+            DecorateTextBlock(resultTextBlock, smallerFont, true);
+            
+            DecorateButton(PlotButton, new SolidColorBrush(Color.FromRgb(58, 0x8B, 0xED)));
+            DecorateButton(ResetButton, new SolidColorBrush(Color.FromRgb(0xFF, 63, 47)));
+
+            DecorateRadioButton(FunctionRadioButton1, 18);
+            DecorateRadioButton(FunctionRadioButton2, 18);
+            DecorateRadioButton(FunctionRadioButton3, 18);
+            DecorateRadioButton(FunctionRadioButton4, 18);
+
+        }
+
+        private void DecorateTextBlock(TextBlock textBlock, int fontSize, bool doCenterText)
+        {
+            textBlock.FontFamily = new FontFamily("Roboto");
+            textBlock.FontSize = fontSize;
+            textBlock.SetValue(TextBlock.FontWeightProperty, FontWeights.Medium);
+            textBlock.TextWrapping = TextWrapping.Wrap;
+
+            if (doCenterText)
+            {
+                textBlock.TextAlignment = TextAlignment.Center;
+            }
+            else
+            {
+                textBlock.TextAlignment = TextAlignment.Left;
+            }
+        }
+
+        private void DecorateButton(Button button, Brush color)
+        {
+            button.FontFamily = new FontFamily("Roboto");
+            button.FontSize = 15;
+            button.Foreground = Brushes.White;
+            button.SetValue(TextBlock.FontWeightProperty, FontWeights.Medium);
+            button.Background = color;
+            button.BorderBrush = color;
+        }
+
+        private void DecorateRadioButton(RadioButton radioButton, int fontSize)
+        {
+            radioButton.FontFamily = new FontFamily("Roboto");
+            radioButton.FontSize = fontSize;
+            radioButton.Height = 30;
+        }
+    }
+}
---
+++ FirstWpfApp/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,50 @@
+﻿namespace FirstWpfApp.ViewModels
+{
+    public class MainWindowViewModel : BaseViewModel
+    {
+        private double _leftBound;
+        private double _rightBound;
+        private double _accuracy;
+        private string _synchronizedText;
+
+        public string SynchronizedText
+        {
+            get => _synchronizedText;
+            set
+            {
+                _synchronizedText = value;
+                OnPropertyChanged(nameof(SynchronizedText));
+            }
+        }
+        public double Leftbound
+        {
+            get => _leftBound;
+            set
+            {
+                _leftBound = value;
+                OnPropertyChanged(nameof(Leftbound));
+            }
+        }
+
+        public double Rightbound
+        {
+            get => _rightBound;
+            set
+            {
+                _rightBound = value;
+                OnPropertyChanged(nameof(Rightbound));
+            }
+        }
+
+        public double Accuracy
+        {
+            get => _accuracy;
+            set
+            {
+                _accuracy = value;
+                OnPropertyChanged(nameof(Accuracy));
+            }
+        }
+        
+    }
+}
